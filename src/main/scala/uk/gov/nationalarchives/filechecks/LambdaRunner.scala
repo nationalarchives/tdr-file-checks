9401c26bb9af4b851857a8c3fc4b568f5cd18fd7
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2026-05-07 22:51:49 +0100

    Update runner

--- src/main/scala/uk/gov/nationalarchives/filechecks/LambdaRunner.scala
+++ src/main/scala/uk/gov/nationalarchives/filechecks/LambdaRunner.scala
@@ -11,14 +11,10 @@ object LambdaRunner extends App {
     |  "consignmentId": "b8caaf81-5ee4-4f0c-a61d-414a20228c16",
     |  "fileId": "44e78491-c85c-44be-888d-fc54acfbc79e",
     |  "originalPath": "object-key.csv",
-    |  "s3SourceBucket": {
-    |    "name": "tdr-source-bucket-intg",
-    |    "objectKey": "898f06e7-1209-4f09-a33f-16d642b6c806/object-key.csv"
-    |  },
-    |  "s3QuarantineBucket": {
-    |    "name": "tdr-quarantine-test",
-    |    "objectKey": "898f06e7-1209-4f09-a33f-16d642b6c806/object-key.csv"
-    |  }
+    |  "s3SourceBucket": "testbucket",
+    |  "s3SourceBucketKey": "Test.docx",
+    |  "s3QuarantineBucket": "quarantineBucket",
+    |  "s3QuarantineBucketKey": "Test.docx"
     |}
     |""".stripMargin
 
