bff2d39765107a563dc9a2f792e80d59a47fea67
ihoyle <ian.hoyle@nationalarchives.gov.uk>
2026-08-24 15:20:25 +0100

    TDRD-1687 Don't use DROID for checksums
    Squashed history and re-signed so every commit on the branch is
    verified (a prior merge commit was unsigned).
    
    Co-authored-by: vimleshtna <104834445+vimleshtna@users.noreply.github.com>
    Co-authored-by: ian-hoyle <5082876+ian-hoyle@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.20"
   lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.477"
-  lazy val s3Utils = "uk.gov.nationalarchives" %% "s3-utils" % "0.1.320"
+  lazy val s3Utils = "uk.gov.nationalarchives" %% "s3-utils" % "0.1.338"
   lazy val typesafe = "com.typesafe" % "config" % "1.4.9"
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
   lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
--- src/main/scala/uk/gov/nationalarchives/filechecks/Lambda.scala
+++ src/main/scala/uk/gov/nationalarchives/filechecks/Lambda.scala
@@ -8,12 +8,14 @@ import io.circe.Printer
 import io.circe.generic.auto._
 import io.circe.parser.decode
 import io.circe.syntax.EncoderOps
-import uk.gov.nationalarchives.aws.utils.s3.S3Clients.{s3, s3Async}
 import uk.gov.nationalarchives.aws.utils.s3.S3Utils
 import uk.gov.nationalarchives.filechecks.ApplicationConfig._
 import uk.gov.nationalarchives.filechecksutils._
+import software.amazon.awssdk.services.s3.S3AsyncClient
 
 import java.io.{InputStream, OutputStream}
+import java.nio.charset.StandardCharsets.UTF_8
+import java.nio.file.{Files, Path, Paths}
 import java.util.UUID
 import scala.io.Source
 import scala.language.postfixOps
@@ -25,37 +27,92 @@ class Lambda {
   private val containerSignature: SignatureFile = SignatureFile(containerSignatureName, containerSignatureVersion)
   private val droidSignature: SignatureFile = SignatureFile(droidSignatureName, droidSignatureVersion)
 
-  private val s3AsyncClient = s3Async(s3Endpoint)
-  private val s3Client = s3(s3Endpoint)
-  private val s3Utils = S3Utils(s3AsyncClient)
-  private val droidFileChecksResultExtractor: DroidFileChecksResultExtractor = DroidFileChecksResultExtractor(containerSignature, droidSignature, s3Client)
-  private val guardDutyScanResultExtractor: GuardDutyScanResultExtractor = GuardDutyScanResultExtractor(s3AsyncClient)
+  private lazy val s3Client: S3AsyncClient = S3ClientProvider.s3AsyncClient
+  private lazy val s3Utils: S3Utils = S3Utils(s3Client)
+  private val droidFileChecksResultExtractor: DroidFileChecksResultExtractor = DroidFileChecksResultExtractor(containerSignature, droidSignature)
+  private lazy val guardDutyScanResultExtractor: GuardDutyScanResultExtractor = GuardDutyScanResultExtractor(s3Client)
 
-  def process(inputBody: InputStream, output: OutputStream): Unit = {
-    val body = Source.fromInputStream(inputBody).getLines().mkString
-    for {
-      fileChecksParameters <- IO.fromEither(decode[FileChecksParameters](body))
-      droidFileChecksResult <- IO.fromEither(extractDroidFileChecksResults(fileChecksParameters))
-      malwareScanResult <- IO(
-        guardDutyScanResultExtractor.getMalwareScanResult(
-          fileChecksParameters.s3SourceBucket,
-          fileChecksParameters.s3SourceBucketKey,
-          pollMalwareScanCompleteAwaitSecs
+  def process(inputBody: InputStream, output: OutputStream): Unit = (for {
+    body <- IO(Source.fromInputStream(inputBody).getLines().mkString)
+    fileChecksParameters <- IO.fromEither(decode[FileChecksParameters](body))
+    fileChecksResult <- processFileChecks(fileChecksParameters)
+    _ <- IO(output.write(fileChecksResult.asJson.printWith(Printer.noSpaces).getBytes(UTF_8)))
+  } yield ())
+    .handleErrorWith { throwable =>
+      IO(logger.error("Error processing file checks", throwable)) *>
+        IO(output.write(Map("error" -> throwable.getMessage).asJson.printWith(Printer.noSpaces).getBytes(UTF_8)))
+    }
+    .unsafeRunSync()
+
+  private def processFileChecks(fileChecksParameters: FileChecksParameters): IO[FileChecksResult] =
+    getObjectSize(fileChecksParameters.s3SourceBucket, fileChecksParameters.s3SourceBucketKey).flatMap { size =>
+      if (size > largeFileThresholdBytes) {
+        val s3FilePath = Paths.get(s3FilesMountPoint, fileChecksParameters.s3SourceBucketKey)
+        IO(logger.info("File {} exceeds threshold ({}B > {}B), using S3 Files mount at {}", fileChecksParameters.s3SourceBucketKey, size, largeFileThresholdBytes, s3FilePath)) *>
+          runFileChecks(fileChecksParameters, s3FilePath)
+      } else {
+        IO(logger.info("File {} under threshold ({}B <= {}B), downloading to local", fileChecksParameters.s3SourceBucketKey, size, largeFileThresholdBytes)) *>
+          withDownloadedLocalFile(fileChecksParameters.s3SourceBucket, fileChecksParameters.s3SourceBucketKey) { localFile =>
+            runFileChecks(fileChecksParameters, localFile)
+          }
+      }
+    }
+
+  private def withDownloadedLocalFile[A](bucket: String, key: String)(fn: Path => IO[A]): IO[A] =
+    Resource
+      .make(IO.blocking(Files.createTempFile("tdr-file-checks-", ".tmp")))(path => IO.blocking(Files.deleteIfExists(path)).void)
+      .use(path => s3Utils.downloadFiles(bucket, key, Some(path)).flatMap(_ => fn(path)))
+
+  private def getObjectSize(bucket: String, key: String): IO[Long] =
+    IO.fromFuture(IO {
+      import scala.jdk.FutureConverters._
+      s3Client
+        .headObject(
+          software.amazon.awssdk.services.s3.model.HeadObjectRequest.builder().bucket(bucket).key(key).build()
         )
-      )
-      _ <- copyToCleanDestinationOrQuarantineBucket(fileChecksParameters, malwareScanResult)
-      result: FileChecksResult = buildFileChecksResult(fileChecksParameters, droidFileChecksResult, malwareScanResult)
-      output <- Resource
-        .fromAutoCloseable(IO(output))
-        .use(outputStream => {
-          outputStream.write(result.asJson.printWith(Printer.noSpaces).getBytes())
-          IO.unit
-        })
-    } yield output
-  }.unsafeRunSync()
+        .asScala
+    }).map(_.contentLength())
 
-  private def buildFileChecksResult(fileChecksParameters: FileChecksParameters, droidFileChecksResult: DroidFileChecksResult, malwareScanResult: MalwareScanResult) = {
-    val checksum = Checksum(fileChecksParameters.fileId, droidFileChecksResult.checksum)
+  private def runFileChecks(fileChecksParameters: FileChecksParameters, filePath: Path): IO[FileChecksResult] =
+    for {
+      parallelResults <- IO.both(
+        runDroidAndChecksumInParallel(fileChecksParameters, filePath),
+        IO.blocking(
+          guardDutyScanResultExtractor.getMalwareScanResult(
+            fileChecksParameters.s3SourceBucket,
+            fileChecksParameters.s3SourceBucketKey,
+            pollMalwareScanCompleteAwaitSecs
+          )
+        ).flatMap { malwareScanResult =>
+          copyToCleanDestinationOrQuarantineBucket(fileChecksParameters, malwareScanResult).as(malwareScanResult)
+        }
+      )
+      ((droidResult, checksumResult), malwareScanResult) = parallelResults
+      result = buildFileChecksResult(fileChecksParameters, droidResult, checksumResult, malwareScanResult)
+    } yield result
+
+  private def runDroidAndChecksumInParallel(fileChecksParameters: FileChecksParameters, filePath: Path): IO[(DroidFileChecksResult, String)] =
+    for {
+      result <- IO.both(
+        IO.blocking {
+          droidFileChecksResultExtractor.fileChecksResult(
+            fileChecksParameters.fileId,
+            fileChecksParameters.originalPath,
+            filePath.toUri
+          )
+        }.flatMap(IO.fromEither),
+        ChecksumCalculator.calculateChecksum(filePath)
+      )
+      (droidResult, checksumResult) = result
+    } yield (droidResult, checksumResult)
+
+  private def buildFileChecksResult(
+      fileChecksParameters: FileChecksParameters,
+      droidFileChecksResult: DroidFileChecksResult,
+      checksumValue: String,
+      malwareScanResult: MalwareScanResult
+  ) = {
+    val checksum = Checksum(fileChecksParameters.fileId, checksumValue)
     val ffidMetadataInputValues = droidFileChecksResult.ffidMetadataInputValues
     val antivirus = Antivirus(
       software = malwareScanResult.software,
@@ -87,15 +144,6 @@ class Lambda {
     }
   }
 
-  private def extractDroidFileChecksResults(fileChecksParameters: FileChecksParameters): Either[Throwable, DroidFileChecksResult] = for {
-    metadata <- droidFileChecksResultExtractor.fileChecksResult(
-      fileChecksParameters.consignmentId,
-      fileChecksParameters.fileId,
-      fileChecksParameters.originalPath,
-      fileChecksParameters.s3SourceBucket,
-      fileChecksParameters.s3SourceBucketKey
-    )
-  } yield metadata
 }
 
 case class FileChecksParameters(
